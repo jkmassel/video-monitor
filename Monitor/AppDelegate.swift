4fa0768a16762031acbe2b96ab088f3c4483092c
Jeremy Massel <jeremy.massel@gmail.com>
2018-04-12 11:17:57 -0600

    Initial Commit

---
+++ Monitor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Monitor
+//
+//  Created by Jeremy Massel on 2018-04-12.
+//  Copyright © 2018 Massel Industries. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Monitor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Monitor
+//
+//  Created by Jeremy Massel on 2018-04-12.
+//  Copyright © 2018 Massel Industries. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
